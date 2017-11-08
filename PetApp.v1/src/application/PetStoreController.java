d3b0d7f13e62b2dd944d07a59eb696bb946e2163
legoute21 <legoute21@gmail.com>
2017-11-08 18:23:21 -0500

    Added Eclipse Java Project

---
+++ PetApp.v1/src/application/PetStoreController.java
@@ -0,0 +1,40 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class PetStoreController {
+
+    @FXML
+    private TextField petName;
+
+    @FXML
+    private AnchorPane displayPane;
+
+    @FXML
+    private TextArea display;
+
+    @FXML
+    private TextField petType;
+
+    @FXML
+    private Button addPet;
+
+    @FXML
+    private Button viewPets;
+
+    @FXML
+    void btnAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnView(ActionEvent event) {
+
+    }
+
+}
